562f21e03cdff7fe8a9142906cd7ea3f06af1860
Justin Crapse <justincrapse@gmail.com>
2020-04-22 22:27:20 -0700

    finished chapter 3

---
+++ ch_3/build_dict.py
@@ -0,0 +1,14 @@
+a = dict(one=1, two=2, three=3)  # least code = nice!
+b = {'one': 1, 'two': 2, 'three': 3}
+c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+d = dict([('two', 2), ('one', 1), ('three', 3)])
+e = dict({'three': 3, 'one': 1, 'two': 2})  # why would you ever do this?
+print(a == b == c == d == e)
+
+# dict comprehensions:
+DIAL_CODES = [(86, 'China'), (91, 'India'), (1, 'United States'), (62, 'Indonesia'), (55, 'Brazil'), (92, 'Pakistan'),
+              (880, 'Bangladesh'), (234, 'Nigeria'), (7, 'Russia'), (81, 'Japan'), ]
+
+country_code = {country: code for code, country in DIAL_CODES}
+print(country_code)
+
